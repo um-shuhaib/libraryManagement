e4804d86a7b2ce61cf6edf252deab84d46d6ef9f
um-shuhaib <shuhaib5560@gmail.com>
2025-11-17 12:24:43 +0530

    context processor updated and dashboard modified (updated the count of due and avl issue count)

--- libraryApp/context_processor.py
+++ libraryApp/context_processor.py
@@ -1,14 +1,18 @@
 from django.db import models
-from libraryApp.models import Book
+from libraryApp.models import Book,Issue
 
 def library_status(request):
     total_books = Book.objects.count()
     total_copies = Book.objects.aggregate(total=models.Sum("total_copy"))["total"] or 0
     avl_books = Book.objects.aggregate(total=models.Sum("avl_copy"))["total"] or 0
+    issued_books=Issue.objects.filter(status="issued").count()
+    overdue_books=Issue.objects.filter(status="overdue").count()
 
 
     return {
         "total_books": total_books,
         "total_copies": total_copies,
         "avl_books": avl_books,
+        "issued_books": issued_books,
+        "overdue_books": overdue_books,
     }
